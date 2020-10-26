92898bdfbfb55036fdb4925ee0d8581e2dcfa990
Vander Lima <vnd.vander@gmail.com>
2020-10-26 16:42:02 -0300

    testes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,14 +2,42 @@ import express from 'express';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-    console.log('listagem de usuarios');
+app.use(express.json())
 
-    response.json([
-        'teste1',
-        'teste23',
-        'teste45'
-    ])
+const users = [
+    'Diego',
+    'Cleiton',
+    'Robson',
+    'Daniel'
+]
+
+app.get('/users', (request, response) => {
+    const search = String(request.query.search)
+
+    const filteredUsers = search ? users.filter(user => user.includes(search)) : users
+
+    return response.json(filteredUsers)
 });
+
+app.get('/users/:id', (request, response) => {
+    const id = Number(request.params.id)
+
+    const user = users[id]
+
+    return response.json(user)
+})
+
+app.post('/users', (request, response) => {
+    const data = request.body;
+
+    console.log(data)
+    
+    const user = {
+        name: data.name,
+        email: data.email
+    }
+
+    return response.json(user)
+})
 
 app.listen(3333);
