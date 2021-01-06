a60e7f728a803e44293a3661617b1bf8b35a79fc
Vander Lima <vnd.vander@gmail.com>
2021-01-06 13:09:52 -0300

    1/2 backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,43 +1,10 @@
-import express from 'express';
+import express from 'express'
+import routes from './routes'
 
 const app = express();
 
 app.use(express.json())
 
-const users = [
-    'Diego',
-    'Cleiton',
-    'Robson',
-    'Daniel'
-]
+app.use(routes)
 
-app.get('/users', (request, response) => {
-    const search = String(request.query.search)
-
-    const filteredUsers = search ? users.filter(user => user.includes(search)) : users
-
-    return response.json(filteredUsers)
-});
-
-app.get('/users/:id', (request, response) => {
-    const id = Number(request.params.id)
-
-    const user = users[id]
-
-    return response.json(user)
-})
-
-app.post('/users', (request, response) => {
-    const data = request.body;
-
-    console.log(data)
-    
-    const user = {
-        name: data.name,
-        email: data.email
-    }
-
-    return response.json(user)
-})
-
-app.listen(3333);
+app.listen(3333)
